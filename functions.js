de5ef79428ccd077e2cf165f4a485b60ffc6d8cc
Jonatanehs <jehernandez257@soy.sena.edu.co>
2023-08-22 09:28:29 -0500

    operaciones simples

---
+++ functions.js
@@ -0,0 +1,32 @@
+// define la funcion sumar(add)
+function add(a,b) {
+    return a + b;
+}
+
+// define la funcion restar(substract)
+function substract(a,b) {
+    return a - b;
+}
+
+// define la funcion multiplicar(multiply)
+function multiply(a,b) {
+    return a * b;
+}
+
+// define la funcion dividir(divide)
+function divide(a,b) {
+    if (b==0 && a==0)  {
+        return "No Aplica"
+    } elseif(b==0);{
+    return "n/a";
+    } 
+    else {
+        return a / b;
+    }
+
+}
+
+exports.add = add;
+exports.substract = substract;
+exports.multiply = multiply;
+exports.divide = divide;
